d4d5331041f30df1eea5f11d226a3db0df9d408a
Philip Schlump <pschlump@gmail.com>
2015-09-07 21:32:00 -0600

    Added Fopen

---
+++ fopen.go
@@ -0,0 +1,26 @@
+package filelib
+
+import (
+	"errors"
+	"os"
+)
+
+var invalidMode = errors.New("Invalid Mode")
+
+// Open a file with a mode string - similar to C fopen
+func Fopen(fn string, mode string) (file *os.File, err error) {
+	file = nil
+	if mode == "r" {
+		file, err = os.Open(fn) // For read access.
+	} else if mode == "w" {
+		file, err = os.OpenFile(fn, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	} else if mode == "a" {
+		file, err = os.OpenFile(fn, os.O_RDWR|os.O_APPEND, 0660)
+		if err != nil {
+			file, err = os.OpenFile(fn, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+		}
+	} else {
+		err = invalidMode
+	}
+	return
+}
