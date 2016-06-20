8dd03d37112d8ccec6656fb7e53d5898ddea0991
Philip Schlump <pschlump@gmail.com>
2016-06-20 17:59:02 -0600

    fixed testing

--- file_test.go
+++ file_test.go
@@ -1,8 +1,19 @@
 package filelib
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func Test_Exists(t *testing.T) {
+
+	os.Mkdir("./test_data", 0700)
+	err := ioutil.WriteFile("./test_data/file-exists", []byte("bob\n"), 0600)
+	if err != nil {
+		t.Errorf("Unable to setup test file, ./test_data/file-exists")
+	}
+
 	if Exists("./no-such-file.do-not-create") {
 		t.Errorf("Expected false, file should not exist\n")
 	}
